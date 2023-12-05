9555b4ef7ac79ba5e34758e3b6a8a63481eef885
Ian Gulliver <github@flamingcow.io>
2023-12-05 14:02:28 -0800

    v1.0.30

--- Formula/bkl.rb
+++ Formula/bkl.rb
@@ -1,8 +1,8 @@
 class Bkl < Formula
   desc "Evaluates bkl configuration layer files"
   homepage "https://bkl.gopatchy.io"
-  url "https://github.com/gopatchy/bkl/archive/refs/tags/v1.0.29.tar.gz"
-  sha256 "8ceab68e715cb7f6b70c43b2eae0a3da84a086457c7d9f15755744a6afd45c6b"
+  url "https://github.com/gopatchy/bkl/archive/refs/tags/v1.0.30.tar.gz"
+  sha256 "a8c2439d39b542eeab8556914e30c4c81e0db00ccec3eba9c6202e6fa1ea5a97"
   license "Apache-2.0"
   head "https://github.com/gopatchy/bkl/", branch: "main"
 
