af5fa972e266cf116fcbb8382db1e496fb07963d
Ian Gulliver <github@flamingcow.io>
2023-12-21 20:04:38 -0800

    Try #4

--- Formula/bkl.rb
+++ Formula/bkl.rb
@@ -1,10 +1,10 @@
 class Bkl < Formula
-  tag = "v1.0.35"
+  @@tag = "v1.0.35"
   sha256 "1055461383be8d459c3ffc058097e3f0f82a1052a30c068fb128ade96c2a2a34"
 
   desc "Evaluates bkl configuration layer files"
   homepage "https://bkl.gopatchy.io"
-  url "https://github.com/gopatchy/bkl/archive/refs/tags/#{tag}.tar.gz"
+  url "https://github.com/gopatchy/bkl/archive/refs/tags/#{@@tag}.tar.gz"
   license "Apache-2.0"
   head "https://github.com/gopatchy/bkl/", branch: "main"
 
@@ -12,6 +12,6 @@ class Bkl < Formula
 
   def install
     ENV["CGOENABLED"] = "0"
-    system "go", "build", "-tags=bkl-#{tag}" "-trimpath", "-ldflags=-extldflags=-static", "-o", "#{bin}/", "./..."
+    system "go", "build", "-tags=bkl-#{@@tag}" "-trimpath", "-ldflags=-extldflags=-static", "-o", "#{bin}/", "./..."
   end
 end
